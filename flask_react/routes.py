37a0d464a49896d9b3f190e5f48fc36553d78782
AmrAbrazek <amr.3brazek@gmail.com>
2023-06-15 01:12:15 +0300

    adding env and home route

--- flask_react/routes.py
+++ flask_react/routes.py
@@ -1,5 +1,13 @@
 from flask import render_template, redirect, url_for, flash
-from flaskproj import app
-from flaskproj.forms import RegistrationForm, LoginForm
+from flask_react import app
+from flask_react.forms import RegistrationForm, LoginForm
 
+
+Navbar = ['home', 'about']
+
+@app.route('/home')
+@app.route('/')
+def home():
+    endpoint_title = 'home'
+    return render_template('home.html', data={ 'title':endpoint_title, 'Navbar':Navbar})
 
